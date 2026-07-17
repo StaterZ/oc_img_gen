81c558592d723ce4f321318d2d52dee545b3b50a
StaterZ <57621744+StaterZ@users.noreply.github.com>
2026-07-17 11:48:07 +0200

    better muxer packet ordering

--- src/encoder/muxer.rs
+++ src/encoder/muxer.rs
@@ -3,7 +3,7 @@ use szu::iter::IteratorExt;
 use crate::{encoder::media_container::{Descriptor, DescriptorContent, MediaFile, Packet}, math::Frac};
 
 pub trait PacketWriter {
-	fn get_next_packet_time(&self) -> Option<Frac<u64>>;
+	fn get_next_packet_end_time(&self) -> Option<Frac<u64>>;
 	fn get_next_packet(&mut self) -> Option<Packet>;
 }
 
@@ -27,7 +27,7 @@ impl Muxer {
 	pub fn process(&mut self, writers: &mut [&mut dyn PacketWriter]) {
 		while let Some(writer) = writers
 			.iter_mut()
-			.min_by_key_while(|writer| writer.get_next_packet_time())
+			.min_by_key_while(|writer| writer.get_next_packet_end_time())
 		{
 			let packet = writer.get_next_packet().unwrap(); //SAFETY: unwrap is safe due to while condition
 			self.output.stream_descs[packet.stream_id as usize].num_packets += 1;
@@ -39,7 +39,7 @@ impl Muxer {
 		while let Some(writer) = writers
 			.iter_mut()
 			.filter_map(|writer| writer
-				.get_next_packet_time()
+				.get_next_packet_end_time()
 				.map(|t| (t, writer)))
 			.min_by_key(|(t, _)| *t)
 			.map(|(_, writer)| writer)
