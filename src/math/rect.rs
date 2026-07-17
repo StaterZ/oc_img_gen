4347ce44b728e7b9d42651e36244af233feb3b68
StaterZ <57621744+StaterZ@users.noreply.github.com>
2026-07-17 11:50:00 +0200

    fix broken cli container size

--- src/math/rect.rs
+++ src/math/rect.rs
@@ -1,3 +1,5 @@
+use std::ops::{Div, DivAssign, Mul, MulAssign};
+
 use szu::math::GoodNum;
 
 use super::{Point, Size};
@@ -19,5 +21,39 @@ impl<T: GoodNum> Rect<T> {
 	pub fn contains(&self, point: Point<T>) -> bool {
 		(self.pos.x..self.pos.x + self.size.w).contains(&point.x) &&
 		(self.pos.y..self.pos.y + self.size.h).contains(&point.y)
+	}
+}
+
+impl<T: GoodNum> Mul<Size<T>> for Rect<T> {
+	type Output = Self;
+
+	fn mul(self, rhs: Size<T>) -> Self::Output {
+		Rect {
+			pos: self.pos * rhs,
+			size: self.size * rhs,
+		}
+	}
+}
+
+impl<T: GoodNum> MulAssign<Size<T>> for Rect<T> {
+	fn mul_assign(&mut self, rhs: Size<T>) {
+		*self = *self * rhs;
+	}
+}
+
+impl<T: GoodNum> Div<Size<T>> for Rect<T> {
+	type Output = Self;
+
+	fn div(self, rhs: Size<T>) -> Self::Output {
+		Rect {
+			pos: self.pos / rhs,
+			size: self.size / rhs,
+		}
+	}
+}
+
+impl<T: GoodNum> DivAssign<Size<T>> for Rect<T> {
+	fn div_assign(&mut self, rhs: Size<T>) {
+		*self = *self / rhs;
 	}
 }
